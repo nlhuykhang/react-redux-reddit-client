26c01fc34c726cb5d642487e5739c5e2063760e8
Khang Nguyen-Le <nlhuykhang@gmail.com>
2017-06-30 04:57:45 +0700

    Update post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Media } from 'react-bootstrap'
+import defaultImage from './I_See_What_You_Did_There.png'
+import isURL from 'is-url'
 
 export default class Post extends Component {
   static propTypes = {
-    title: PropTypes.string
+    title: PropTypes.string,
+    selftext: PropTypes.string,
+    thumbnail: PropTypes.string,
+    thumbnail_height: PropTypes.number,
+    thumbnail_width: PropTypes.number,
   }
 
   render () {
     const {
       title,
+      selftext,
+      thumbnail,
+      thumbnail_height: thumbnailHeight,
+      thumbnail_width: thumbnailWidth,
     } = this.props
 
+    console.log(this.props.thumbnail)
+
     return (
-      <div className=''>
-        <h4 className=''>{title}</h4>
-      </div>
+      <Media>
+        <Media.Left>
+          <img
+            src={isURL(thumbnail) ? thumbnail : defaultImage}
+            width={thumbnailWidth || 140}
+            height={thumbnailHeight || 140}
+            />
+        </Media.Left>
+        <Media.Body>
+          <Media.Heading>{title}</Media.Heading>
+          <p>{selftext}</p>
+        </Media.Body>
+      </Media>
     )
   }
 }
